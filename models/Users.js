518e28670636dfce322007ce06493599c67d4767
vitaliy <tolikumc@gmail.com>
2019-12-11 16:06:36 +0200

    [Update] create User model

---
+++ models/Users.js
@@ -0,0 +1,45 @@
+
+
+modeule.exports = (sequelize, Datatypes) =>{
+    return sequelize.define('user', {
+        id:{
+            type: Datatypes.UUID,
+            primaryKey: true
+        },
+        first_name:{
+            type: Datatypes.STRING,
+            isAlphanumeric: true,
+            require: true,
+            allowNull: false
+        },
+        last_name:{
+            type: Datatypes.STRING,
+            require: true,
+            allowNull: false
+        },
+        user_name:{
+            type: Datatypes.STRING,
+            require: true,
+            allowNull: false,
+            len: [5, 20]
+        },
+        password:{
+            type: Datatypes.STRING,
+            require: true,
+            allowNull: false,
+            len: [5, 20]
+        },
+        email:{
+            type: Datatypes.STRING,
+            require: true,
+            allowNull: false,
+            isEmail: true,
+            len: [7, 100]
+        },
+        update_at:{type: Datatypes.DATETIME},
+        delete_at:{type: Datatypes.DATETIME},
+    },{
+        underscored: true,
+        paranoid: true
+    })
+};
